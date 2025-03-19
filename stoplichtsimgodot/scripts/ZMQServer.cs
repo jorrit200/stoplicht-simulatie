629f8611c4aa74d0f80dacd8f9bfa45f4af3acee
JoostBakker28 <113503680+JoostBakker28@users.noreply.github.com>
2025-03-19 09:58:16 +0100

    ZMQ setup

---
+++ stoplichtsimgodot/scripts/ZMQServer.cs
@@ -0,0 +1,40 @@
+using Godot;
+using NetMQ;
+using NetMQ.Sockets;
+using System.Threading;
+
+public partial class ZeroMQServer : Node
+{
+    private Thread _serverThread;
+    private bool _isRunning = true;
+
+    public override void _Ready()
+    {
+        // Start de server in een aparte thread
+        _serverThread = new Thread(ServerLoop);
+        _serverThread.Start();
+    }
+
+    private void ServerLoop()
+    {
+        using (var server = new ResponseSocket("@tcp://*:5555")) // Luistert op poort 5555
+        {
+            GD.Print("ZeroMQ Server gestart op poort 5555...");
+            
+            while (_isRunning)
+            {
+                if (server.TryReceiveFrameString(out string message))
+                {
+                    GD.Print("Ontvangen bericht: " + message);
+                    server.SendFrame("Bericht ontvangen: " + message);
+                }
+            }
+        }
+    }
+
+    public override void _ExitTree()
+    {
+        _isRunning = false;
+        _serverThread.Join();
+    }
+}
---
+++ stoplichtsimgodot/scripts/ZMQclient.cs
@@ -0,0 +1,20 @@
+using Godot;
+using NetMQ;
+using NetMQ.Sockets;
+
+public partial class ZeroMQClient : Node
+{
+    public override void _Ready()
+    {
+        GD.Print("Verbinding maken met ZeroMQ Server...");
+        using (var client = new RequestSocket(">tcp://localhost:5555"))
+        {
+            client.SendFrame("Hallo, ZeroMQ Server!");
+            
+            if (client.TryReceiveFrameString(out string response))
+            {
+                GD.Print("Antwoord van server: " + response);
+            }
+        }
+    }
+}
