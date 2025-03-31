0f8512dcbed0864176ecb97aa5b4a4facc70e359
JoostBakker28 <113503680+JoostBakker28@users.noreply.github.com>
2025-03-31 12:23:12 +0200

    Update ZMQPublisher.cs

--- stoplichtsimgodot/scripts/ZMQPublisher.cs
+++ stoplichtsimgodot/scripts/ZMQPublisher.cs
@@ -9,7 +9,7 @@ public partial class ZMQPublisher : Node
 	public override void _Ready()
 	{
 		publisher = new PublisherSocket();
-		publisher.Bind("tcp://127.0.0.1:5556"); // Bind naar poort
+		publisher.Bind("tcp://192.168.91.69:5556"); // Bind naar poort
 
 		GD.Print("Publisher gestart op poort 5556...");
 
@@ -23,7 +23,7 @@ public partial class ZMQPublisher : Node
 		while (true)
 		{
 			// Stuur berichten naar een topic
-			publisher.SendMoreFrame("sensoren_rijbaan").SendFrame("\"1.1\": {\"voor\": false,\"achter\": false}");
+			publisher.SendMoreFrame("sensoren_rijbaan").SendFrame("{\"1.1\": {\"voor\": false,\"achter\": false}}");
 
 			// Wacht even tussen berichten
 			System.Threading.Thread.Sleep(1000);  // Bericht elke seconde
