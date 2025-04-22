d753a6f04108e68a735658b8015013819bd1276d
JoostBakker28 <113503680+JoostBakker28@users.noreply.github.com>
2025-04-22 15:44:00 +0200

    Startup fietsers

---
+++ stoplichtsimgodot/scripts/Cyclist.cs
@@ -0,0 +1,12 @@
+namespace StoplichtSimGodot.scripts;
+
+public partial class Cyclist : Vehicle
+{
+	public Cyclist()
+	{
+		Speed = 20;
+		RoadToUse = "BikeRoad";
+		IsMoving = true;
+		SpawnChance = 15;
+	}
+}
