1411ebfb577d8b442d54f57a3804c19f900e3ff1
jorrit <jorritdehaan200@gmail.com>
2025-05-13 11:22:28 +0200

    Bridge sensor topic json formatting

--- stoplichtsimgodot/scripts/Control/BridgeDrawer.cs
+++ stoplichtsimgodot/scripts/Control/BridgeDrawer.cs
@@ -9,65 +9,65 @@ namespace StoplichtSimGodot.scripts.Control;
 
 public partial class BridgeDrawer : Sprite2D
 {
-	[Export] private TrafficLight _trafficLight;
-	[Export] private AnimationPlayer _animationPlayer;
-	[Export] private ZmqPublisher _zmqPublisher;
+    [Export] private TrafficLight _trafficLight;
+    [Export] private AnimationPlayer _animationPlayer;
+    [Export] private ZmqPublisher _zmqPublisher;
 
-	private BridgeState _bridgeState;
+    private BridgeState _bridgeState;
 
-	[Signal]
-	public delegate void BridgeStateChangedEventHandler(BridgeState newState);
+    [Signal]
+    public delegate void BridgeStateChangedEventHandler(BridgeState newState);
 
-	[Export]
-	public BridgeState BridgeState
-	{
-		get => _bridgeState;
-		set
-		{
-			if (value == _bridgeState) return;
-			_bridgeState = value;
-			EmitSignal(SignalName.BridgeStateChanged, Variant.From(value));
-		}
-	}
+    [Export]
+    public BridgeState BridgeState
+    {
+        get => _bridgeState;
+        set
+        {
+            if (value == _bridgeState) return;
+            _bridgeState = value;
+            EmitSignal(SignalName.BridgeStateChanged, Variant.From(value));
+        }
+    }
 
-	public override void _Ready()
-	{
-		_trafficLight.StateChanged += (_, newState) =>
-		{
-			switch (newState)
-			{
-				case TrafficLightState.Green:
-					_animationPlayer.Play("BridgeOpen");
-					break;
-				case TrafficLightState.Red:
-					_animationPlayer.PlayBackwards("BridgeOpen");
-					break;
-				case TrafficLightState.Orange:
-					break;
-				default:
-					throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
-			}
-		};
+    public override void _Ready()
+    {
+        _trafficLight.StateChanged += (_, newState) =>
+        {
+            switch (newState)
+            {
+                case TrafficLightState.Green:
+                    _animationPlayer.Play("BridgeOpen");
+                    break;
+                case TrafficLightState.Red:
+                    _animationPlayer.PlayBackwards("BridgeOpen");
+                    break;
+                case TrafficLightState.Orange:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+            }
+        };
 
-		BridgeStateChanged += state =>
-		{
-			_zmqPublisher.Send("sensoren_bruggen", 
-				$"\"81\": {{\"state\": \"{state.ToString()}\""
-			);
-		};
+        BridgeStateChanged += state =>
+        {
+            _zmqPublisher.Send("sensoren_bruggen",
+                $"{{\"81\": {{\"state\": \"{state.ToString()}\"}}}}"
+            );
+        };
 
-		base._Ready();
-	}
+        base._Ready();
+    }
 }
 
 public class BridgeStateContainer
 {
-	[JsonProperty("state")] public BridgeState State { get; set; }
+    [JsonProperty("state")] public BridgeState State { get; set; }
 }
 
 public enum BridgeState
 {
-	open,
-	dicht,
-	onbekend
+    open,
+    dicht,
+    onbekend
 }
